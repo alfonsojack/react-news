8ccfdf8dd2ab7064933a842122c052317cb3654f
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 16:29:26 -0500

    Set up props

--- src/Article.js
+++ src/Article.js
@@ -1,10 +1,13 @@
 import './Article.css'
 
 
-function Article () {
+function Article ({ selectedStory }) {
   return (
     <div>
-
+      <h1>{selectedStory.title}</h1>
+      <h2>{selectedStory.author}</h2>
+      <img src={selectedStory.urlToImage}/>
+      <p>{selectedStory.content}</p>
     </div>
   )
 }
