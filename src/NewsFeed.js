acf8a9bfbd32ca350849b6176966b0cbc5e6a7b0
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 16:01:35 -0500

    Add filter to clean data

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -7,23 +7,29 @@ const [stories, setStories] = useState([])
 
 const getTopStories = () => {
   return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=6407e766ab484afd83a05c0442070e2b`)
-  .then(response => {
-    if (response.status === 500) {
-      throw new Error('No stories found');
-    }
-    if (!response.ok) {
-      throw new Error(`${response.status}: ${response.message}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('news data', data.articles)
-    return setStories(data.articles);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-}
+    .then(response => {
+      if (response.status === 500) {
+        throw new Error('No stories found');
+      }
+      if (!response.ok) {
+        throw new Error(`${response.status}: ${response.message}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('news data', data.articles);
+
+      const cleanedData = data.articles.filter(article =>
+        article.content && article.author && article.urlToImage && article.description
+      );
+      
+      setStories(cleanedData);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
+
 
 
 useEffect(() => {
