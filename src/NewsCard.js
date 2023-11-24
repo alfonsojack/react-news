0afa00034f660fb2e16d745faeca9a30421deae3
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-24 09:22:07 -0500

    Add expand view option

--- src/NewsCard.js
+++ src/NewsCard.js
@@ -2,18 +2,38 @@ import './NewsCard.css'
 import { useState } from 'react'
 import { Routes, Route, NavLink, Link } from 'react-router-dom';
 
-function NewsCard ({ story, onClick }) {
-  console.log('story', story)
-  const handleClick = () => {
-    onClick(story);
-  };
+function NewsCard ({ story }) {
+  console.log('story', story);
+  const [moreInfo, setMoreInfo] = useState(false);
+  const originalDate = new Date(story.publishedAt);
+  const formattedDate = `${originalDate.getMonth() + 1}/${originalDate.getDate()}/${originalDate.getFullYear()}`;
 
+console.log(formattedDate);
+
+  const handleSeeMoreClick = () => {
+    setMoreInfo((prevValue) => !prevValue);
+  };
   
   return (
-    <div className='NewsCard' onClick={handleClick}>
+    <div className='NewsCard' >
+      <p>{formattedDate}</p>
       <p>{story.source.name}</p>
       <p>{story.title}</p>
       <img className="story-image" src={story.urlToImage} />
+      <p>{story.description}</p>
+      <div>
+        {!moreInfo ? (
+          <button onClick={handleSeeMoreClick}>Expand</button>
+        ) :
+          <div>
+            <p>{story.author}</p>
+            <p>{story.content.replace(/\[.*?\]/g, '')}<Link to={story.url} target="_blank" rel="noopener noreferrer">
+        [Read full article]
+      </Link></p>
+            <button onClick={handleSeeMoreClick}>Minimize</button>
+          </div>
+        }
+      </div>
     </div>
   )
 }
