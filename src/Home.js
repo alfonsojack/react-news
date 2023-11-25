0c9f15667d400847c0f91cf0abf801dd98b88e93
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-24 18:09:15 -0700

    Edit rendering

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,50 @@
 import './Home.css'
-import { useState } from 'react'
-import NewsFeed from './NewsFeed'
+import React, { useState, useEffect } from 'react';
+import Article from './Article';
+import NewsFeed from './NewsFeed';
 
-function Home () {
-  
-  
+function Home() {
+  const [stories, setStories] = useState([]);
+
+  const getTopStories = () => {
+    return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=6407e766ab484afd83a05c0442070e2b`)
+      .then(response => {
+        if (response.status === 500) {
+          throw new Error('No stories found');
+        }
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.message}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('news data', data.articles);
+
+        const cleanedData = data.articles.filter(article =>
+          article.content && article.author && article.urlToImage && article.description && !article.author.includes('<') && !article.author.includes('/')
+        );
+        console.log('hello', cleanedData)
+        setStories(cleanedData);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  useEffect(() => {
+    getTopStories();
+  }, []);
+
+  const handleSearch = (newStories) => {
+    setStories(newStories);
+  };
+
   return (
-    <section>
-      <NewsFeed/>
+    <section className='Home'>
+      <Article onSearch={handleSearch} />
+      <NewsFeed stories={stories} />
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
