97eae36333f8076c4519897cda51c548eb1a38d6
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-25 17:49:21 -0700

    Add color scheme

--- src/Article.js
+++ src/Article.js
@@ -84,7 +84,7 @@ function Article({ onSearch, onCategoryClick }) {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button className='search-button' onClick={handleSearch}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-search"><circle cx="10" cy="10" r="7" className="primary"/><path className="secondary" d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg></button>
+        <button className='search-button' onClick={handleSearch}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-search"><circle cx="10" cy="10" r="7" className="primary"/><path className="search-secondary" d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg></button>
       </div>
       <div className='filter-container'>
         <p className='filter-title'>Categories</p>
--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,25 +1,30 @@
-import './ErrorPage'
+import './ErrorPage.css'
 import { Routes, Route, NavLink, Link } from 'react-router-dom';
 
 function ErrorPage ({ message }) {
   return (
-    <div>
-      <h1>{message}</h1>
+    <div className='page'>
+      <h1 className='error-msg'>{message}</h1>
       <div>
         {message.includes('exist') ? (
           <div>
             <h2>Try going home</h2>
-            <img src='https://t4.ftcdn.net/jpg/02/76/90/37/360_F_276903704_R5meF5b05TBX8BNyuy1siuqMDJwP6kMN.jpg'/>
-            </div>
+            {/* <div className='error-container'> */}
+              <img className='error-img' src='https://t4.ftcdn.net/jpg/02/76/90/37/360_F_276903704_R5meF5b05TBX8BNyuy1siuqMDJwP6kMN.jpg'/>
+              {/* <div className='error-overlay'></div>
+            </div> */}
+          </div>
         ) : (
           <div>
           <h2>Try going home or come back later if the problem persists</h2>
-          <img src='https://us.123rf.com/450wm/lineartist/lineartist1907/lineartist190700815/127345150-dropping-a-letter-in-trash-bin-retro-postman-cartoon-courier-guy-vector-illustration.jpg?ver=6'/>
+          <img className='error-img' src='https://us.123rf.com/450wm/lineartist/lineartist1907/lineartist190700815/127345150-dropping-a-letter-in-trash-bin-retro-postman-cartoon-courier-guy-vector-illustration.jpg?ver=6'/>
           </div>
         )
         }
       </div>
-      <Link to='/'>Home</Link>
+      <Link className='home-btn' to='/'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-home"><path class="home-primary" d="M9 22H5a1 1 0 0 1-1-1V11l8-8 8 8v10a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1zm3-9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/><path className="home-secondary" d="M12.01 4.42l-8.3 8.3a1 1 0 1 1-1.42-1.41l9.02-9.02a1 1 0 0 1 1.41 0l8.99 9.02a1 1 0 0 1-1.42 1.41l-8.28-8.3z"/></svg>
+      </Link>
     </div>
   )
 }
